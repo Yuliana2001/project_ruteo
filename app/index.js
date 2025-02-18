46658e15cbf36f1d977677a8bb77089415bc05a8
Yuliana2001 <yulianaalzatepalacio2001@gmail.com>
2025-02-18 14:35:41 -0500

    logeo

---
+++ app/index.js
@@ -0,0 +1,26 @@
+//importar la librería express
+import express from "express"
+
+//porque agregué type module
+import path from 'path'
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+
+
+//creacion de servidor
+const app = express();
+const PORT = process.env.PORT || 4000;
+app.set("port", PORT);
+
+
+//configuracion a la app para que se pueda acceder a todos los archivos de public como static
+app.use(express.static(__dirname + "/public"));
+
+
+//se hace correr el servidor node con los archivos js
+app.listen(app.get("port"));
+console.log("servidor corriendo en ", app.get("port"));
+
+//para que no aparezca el cannot Get
+app.get("/", (req,res) => res.sendFile(__dirname+"/pages/login.html"));
+app.get("/register", (req,res) => res.sendFile(__dirname+"/pages/register.html"));
