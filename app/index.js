d14d2531cd857c0b284d2aa91ecc329316d91ad0
Yuliana2001 <yulianaalzatepalacio2001@gmail.com>
2025-02-20 08:32:10 -0500

    `Added routes for login, register and mapa pages, and updated login.html to include a title`

--- app/index.js
+++ app/index.js
@@ -21,6 +21,7 @@ app.use(express.static(__dirname + "/public"));
 app.listen(app.get("port"));
 console.log("servidor corriendo en ", app.get("port"));
 
-//para que no aparezca el cannot Get
+//para que no aparezca el cannotGet
 app.get("/", (req,res) => res.sendFile(__dirname+"/pages/login.html"));
 app.get("/register", (req,res) => res.sendFile(__dirname+"/pages/register.html"));
+app.get("/mapa", (req,res) => res.sendFile(__dirname+"/pages/mapa.html"));
---
+++ app/public/generar_mapa.py
@@ -0,0 +1,28 @@
+
+import pandas as pd
+import geopandas as gpd
+import contextily as cx
+import folium
+# Cargar el dataset de zonas WiFi en Medellín
+# Cargar el dataset de zonas WiFi en Medellín
+def reproject(gdf, crs_in):
+  #Crea una copia del geodataframe para no modificar el original
+  gdf_copy = gdf.copy()
+  #Reproyecta el geodataframe al sistema de coordenadas dado
+  gdf_copy.to_crs(crs_in, inplace=True)
+  return gdf_copy
+
+
+path = 'app/public/shp_rutas_de_transporte_publi.zip'
+zonas_metro = gpd.read_file(path)
+m = folium.Map(location=[6.2442, -75.5812], zoom_start=13)
+
+zonas_metro_4326 = reproject(zonas_metro, 4326)
+print(zonas_metro_4326.head())
+
+# Agregar un marcador de ejemplo
+for idx, row in zonas_metro_4326.iterrows():
+    print(row.geometry.y)
+
+# Guardar el mapa como un archivo HTML
+m.save("app/pages/mapa.html")
